00169e280d6bda4b674bb8303acaa83431931c9c
thombanks <thomas.banks@wearecube3.com>
2017-08-21 17:18:06 +0100

    mobile nav

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const navClose = document.querySelector('#nav-close')
 const navModal = document.querySelector('#nav-modal')
 navOpen.addEventListener('click', ()=>{
   navModal.setAttribute('data-state', 'is-active')
+  document.documentElement.style.overflow = 'hidden'
 })
 navClose.addEventListener('click', ()=>{
   navModal.setAttribute('data-state', 'not-active')
+  document.documentElement.style.overflow = 'auto'
 })
